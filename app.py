bc3a1a7dc8bcece14995d5088254a7ff5d622c0c
John Oliver <johnoliver616@yahoo.com>
2024-08-05 19:30:05 -0500

    updated data

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ with header_col1:
 
 
 with header_col2:
-    image_path = "img/nyt.png"  # Update this path to your image file
+    image_path = "img/nyt.png"
     logo_image = Image.open(image_path)
     st.image(logo_image, use_column_width=True)
     st.write("")
@@ -37,16 +37,6 @@ with left_column:
         "Choose An Option",
         ["Select an option", "Monthly Competitions", "Enter Daily Scores", "Player Statistics"]
     )
-
-    # if option == "Enter Daily Scores":
-    #     image_path1 = "img/tucker.jpeg"  
-    #     st.image(image_path1, use_column_width=True)  
-
-    # elif option == "Monthly Competitions":
-    #     image_path2 = "img/sufi.jpeg"
-    #     image2 = Image.open(image_path2)
-    #     rotated_image2 = image2.rotate(270, expand=True)
-    #     st.image(rotated_image2, use_column_width=True)
 
 with spacer:
     st.empty()
